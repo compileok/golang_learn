a13a19b3e30780a03fd6030a5af219aa9a4ff0cc
deming <1425920291@qq.com>
2019-03-10 22:33:18 +0800

    imterface

--- retriever/main.go
+++ retriever/main.go
@@ -1,18 +1,24 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"golang_learn/retriever/mock"
+	"golang_learn/retriever/real"
+)
 type Retriever interface {
 	Get(url string) string
 }
 
 func download(r Retriever) string {
-	return r.Get("www.so.com")
+	return r.Get("http://www.so.com")
 }
 
 func main() {
 	var r Retriever
-	r = mock.Retriever{""}
+	r = mock.Retriever{" it is a fake impl"}
+	fmt.Println(download(r))
+
+	r = real.Retriever{}
 	fmt.Println(download(r))
 
 }
